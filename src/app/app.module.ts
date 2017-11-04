1c02ee2b2aa47d82bbb4388346ea009032cb450b
Julio Romero <julioromero@Julios-MacBook-Pro.local>
2017-11-04 13:18:45 -0400

    subida de header

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { routing, appRoutingProviders } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login.component';
+import { RegisterComponent } from './components/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  providers: [
+    appRoutingProviders
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+    selector: 'login',
+    templateUrl: '../views/login.html',
+})
+export class LoginComponent implements OnInit
+{
+    public title: string;
+
+    constructor(
+        //private _route: ActivatedRoute,
+        //private _router: Router
+    )
+    {
+        this.title = 'Componente de login';
+    }
+
+    ngOnInit()
+    {
+        console.log('El componente login.component ha sido cargado');
+    }
+
+}
